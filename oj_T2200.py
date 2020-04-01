4eda8665a34032825384c4eda08c280bfc88ca45
Steven Tsao <876170495@qq.com>
2020-04-01 22:57:18 +0800

    update oj_T2200.py #002

--- oj_T2200.py
+++ oj_T2200.py
@@ -1,8 +1,11 @@
 # Script Name       : oj_T2200.py
 # Author            : Styphon
 # Created           : 4/1/2020
-# Version           : 1.0
+# Version           : 1.1
 
 n = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
+oA = 0
+eB = 0
+for i in A:
